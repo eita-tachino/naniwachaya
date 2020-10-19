62798a772850690a4cf6be2b6957ad006a9ee541
eita-tachino <34756983+eita-tachino@users.noreply.github.com>
2020-10-19 13:20:12 +0900

    initial commit

---
+++ src/constants/services.js
@@ -0,0 +1,37 @@
+import React from "react"
+import {
+  GiFuji,
+  GiDragonBalls,
+  GiPathDistance,
+  GiChickenOven,
+} from "react-icons/gi"
+export default [
+  {
+    id: 1,
+    icon: <GiFuji className="icon" />,
+    label: "mission",
+    text:
+      " 風もゴーシュの息お母さん汁をおっかさんを出し壁だた。またどっかり無理ないたて交響たまし。",
+  },
+  {
+    id: 2,
+    icon: <GiDragonBalls className="icon" />,
+    label: "vision",
+    text:
+      " かぶれはからだなんどたり何をこすりからはじめたら。猫はセロへまたと鳴っていっしょを虎のようへふくて先生で云ってすっかりセロをしと来いた。",
+  },
+  {
+    id: 3,
+    icon: <GiChickenOven className="icon" />,
+    label: "foods | drinks",
+    text:
+      "ドレミファ、そこのあと。持っ。」私も夜のときの思わずこんどのところを呆れたな。用はゴーシュにまっ糸を行って硝子がいっしょが弾きてどんと夜教えるられるた限りがききうない。",
+  },
+  {
+    id: 4,
+    icon: <GiPathDistance className="icon" />,
+    label: "history",
+    text:
+      "どうしてようよう朝飯をかっこうをしだな。おまえすぐに形をぶっつかってかっこうにきかせなた。わくをもごたた。「狸からあけませ。",
+  }, 
+]
